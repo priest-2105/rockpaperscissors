1043fbd42eddb7a3c389eb3a60fa4e1ed319c749
priest-2105 <fawzybailey782@gmail.com>
2024-08-25 17:37:56 +0100

    js init ✔✔✨

---
+++ super.js
@@ -0,0 +1,22 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.scoreBoard');
+const result_div = document.querySelector('.result');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+
+
+rock_div.addEventListener('click', function() {
+    console.log('Hey you clicked on rock'); 
+})
+
+paper_div.addEventListener('click', function() {
+    console.log('Hey you clicked on paper');  
+})
+
+scissors_div.addEventListener('click', function() {
+    console.log('Hey you clicked on scissors');
+})
